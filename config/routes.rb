9e57c806cda50de4d0b332660313747306e32267
Ivan Porollo <ivanporollo@Ivans-Mac.local>
2017-03-03 19:27:34 -0800

    changed POST to GET

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
-	#get '/spotify/callback' => 'users#callback_hit'
-	post '/spotify/callback' => 'users#callback_hit'
-
+	get '/spotify/callback' => 'users#callback_hit'
+	
 
 end
