b7ecc83b67363036d276cb5294660db4fb997978
Ivan Porollo <ivanporollo@Ivans-Mac.local>
2017-03-04 23:08:59 -0800

    correcting route call

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-	get '/user/givetoken/:id' => 'users#give_token'
+	get '/user/givetoken' => 'users#give_token'
 	get "/user/justcreatedrailsid" => 'users#give_rails_uid'
 	get '/spotify/callback' => 'callbacks#spotify_callback'
 
