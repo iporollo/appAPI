3e837d6f83eff708c093e652643d17052b6384c9
Ivan Porollo <ivanporollo@Ivans-Mac.local>
2017-03-09 14:41:22 -0800

    refresh token implemetation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,32 @@ class UsersController < ApplicationController
 
 
 	def give_token
+
+		client_id = "ea09225ef2974242a1549f3812a15496"
+		client_secret = "49df12072ddf49e7b176bf15b2eef15a"
+
+		encodedClientID = Base64.encode64(client_id)
+		encodedClientSecret = Base64.encode64(client_secret)
+
 		spotifyID = params[:user]
 		currentUser = User.find_by spotify_user_id: spotifyID
-		puts currentUser.token_expiration_time
-		#I need to add a check here for access token expiration
+		
+		if(Time.now > currentUser.token_expiration_time)
+
+			refreshResponse = HTTParty.post("https://accounts.spotify.com/api/token", 
+				:headers => {"Authorization"=> "Bearer #{encodedClientID}:#{encodedClientSecret}"},
+				:query => {:grant_type => "refresh_token", 
+						   :refresh_token => currentUser.refresh_token})
+
+			parsedRefreshResponse = JSON.parse(refreshResponse.body)
+
+			puts parsedRefreshResponse
+
+			currentUser.update_attribute(:access_token, parsedRefreshResponse["access_token"])
+			currentUser.update_attribute(:token_expiration_time, Time.now + parsedRefreshResponse["expires_in"])
+
+		end
+
 		render :json => {:token => currentUser.access_token}
 	end
 
