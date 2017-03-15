634f450360338b9a10b0fd5767663d35ad97933a
Ivan Porollo <ivanporollo@Ivans-Mac.local>
2017-03-14 20:49:40 -0700

    creating party in db implemented

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -14,8 +14,15 @@ class PartiesController < ApplicationController
 	def create
 
 		party = params[:party]
+		partyOwner = User.find_by spotify_user_id: party["host_spotify_id"]
 
-		puts party["name"]
+		Party.create(:name => party["name"],
+					 :playlistID => party["playlist_id"],
+					 :partyID => party["party_id"],
+					 :host_id => party["host_spotify_id"],
+					 :guest_song_add_state => party["guest_song_add_state"],
+					 :down_vote_state => party["down_vote_state"],
+					 :user_id => partyOwner.id)
 
 	end
 
