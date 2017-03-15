d706209b51b16d2c351a1f99cfd9794d91ba93ca
Ivan Porollo <ivanporollo@Ivans-Mac.local>
2017-03-14 22:09:41 -0700

    more debugs again

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -17,10 +17,9 @@ class PartiesController < ApplicationController
 
 		partyOwner = User.find_by spotify_user_id: params[:host_spotify_id]
 
-		Party.create(:name => params[:name],
-					 :playlistID => params[:playlist_id],
-					 :partyID => params[:party_id],
-					 :host_id => params[:host_spotify_id],
+		Party.create(:name => params[:name], 
+			:playlistID => params[:playlist_id],
+			:partyID => params[:party_id],
 					 :guest_song_add_state => params[:guest_song_add_state],
 					 :down_vote_state => params[:down_vote_state],
 					 :user_id => partyOwner.id)
